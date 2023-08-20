bd606236b7ca975774b480e44627bba6b22de50c
hari64 <hari64boli64@gmail.com>
2023-08-20 15:20:35 +0900

    add 2015_2

--- 情報理工/2015/2.py
+++ 情報理工/2015/2.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-for n in [2, 5, 10]:
-    for seed in range(10):
+for n in range(2, 10 + 1):
+    for seed in range(1000):
         np.random.seed(seed)
         print(f"{n=} {seed=}")
 
-        # 確率変数
+        # random variables
         Xs = np.random.random() * np.random.random(n) + np.random.random()
         Ys = np.random.random() * np.random.random(n) + np.random.random()
         Zs = np.random.random() * np.random.random(n) + np.random.random()
 
-        # 正規化(分散を1に)
+        # normalization (mean=0, var=1)
         Xs /= np.std(Xs)
         Ys /= np.std(Ys)
         Zs /= np.std(Zs)
@@ -18,7 +18,7 @@ for n in [2, 5, 10]:
         assert np.isclose(np.var(Ys), 1)
         assert np.isclose(np.var(Zs), 1)
 
-        # 相関係数
+        # correlation coefficients
         rXY = np.corrcoef(Xs, Ys)[0, 1]
         rYZ = np.corrcoef(Ys, Zs)[0, 1]
         rXZ = np.corrcoef(Xs, Zs)[0, 1]
@@ -27,7 +27,14 @@ for n in [2, 5, 10]:
 
         print(f"{np.arccos(rXY)=} {np.arccos(rYZ)=} {np.arccos(rXZ)=}")
 
-        # 以下はn=2では一致する
-        assert np.isclose(
-            rXZ, rXY * rYZ - np.sqrt(1 - rXY**2) * np.sqrt(1 - rYZ**2)
-        ), (rXY, rXY * rYZ - np.sqrt(1 - rXY**2) * np.sqrt(1 - rYZ**2))
+        # the following assertion is true only when n = 2
+        if n == 2:
+            assert np.isclose(
+                rXZ, rXY * rYZ - np.sqrt(1 - rXY**2) * np.sqrt(1 - rYZ**2)
+            )
+
+        # the following assertion is always true
+        EPS = 1e-6
+        thetaMin = np.arccos(rYZ) - np.arccos(rXY)
+        thetaMax = np.arccos(rYZ) + np.arccos(rXY)
+        assert thetaMin - EPS <= np.arccos(rXZ) <= thetaMax + EPS
